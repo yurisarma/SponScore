90961e1df6f00d741a34fb789b3b23a66f725144
yurisarma <yurisarma@gmail.com>
2026-02-15 16:34:49 -0600

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ from scoring import compute_company_profile
 
 app = FastAPI()
 
+@app.get("/")
+def root():
+    return {"message": "SponScore API is live"}
+
 @app.get("/company/{company_name}")
 def get_company(company_name: str):
     result = compute_company_profile(company_name)
@@ -12,7 +16,4 @@ def get_company(company_name: str):
 
     return result
 
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000)
 
