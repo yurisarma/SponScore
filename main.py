d7320417b80be4bbdad473aae79db1bc3441535a
yurisarma <yurisarma@gmail.com>
2026-02-15 20:05:59 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from scoring import compute_company_profile
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # allow all origins for now
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def root():
@@ -15,5 +24,6 @@ def get_company(company_name: str):
         return {"error": "Company not found"}
 
     return result
+
 
 
